4be21caa7444a20ffde326b7a93333c7a60e746a
SchoolLotus <rhys.morris1@education.nsw.gov.au>
2026-02-11 16:53:43 +1100

    added one line anmd didnt work, no new errors so ill just keep it there unlesss it makes an error itself

--- app.py
+++ app.py
@@ -2,14 +2,14 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 #
 # App Configuration
 #
 
-app = Flask(__name__)
-app.secret_key = 'mizuenasekaimiku' # Change this to a random secret key in production
+app = Flask(__name__, template_folder='template')
+app.secret_key = 'secretkeyandstuff' # Change this to a random secret key in production
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tastetracker_ps.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -25,7 +25,7 @@ class User(db.Model):
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.Date, default=date.time.utcnow)
+    created_at = db.Column(db.Date, default=datetime.utcnow)
 
     #User review relationship
     #one user can have mutliple reviews
@@ -42,4 +42,18 @@ class Review(db.Model):
     review_date = db.Column(db.Integer, nullable=False)
     rating = db.Column(db.Integer, nullable=False)
     review_text = db.Column(db.Text, nullable=True)
-    created_at = db.Column(db.Date, default=date.time.utcnow)
+    created_at = db.Column(db.Date, default=datetime.utcnow)
+
+#Home Route - View All Reviews
+
+@app.route("/")
+def Home():
+     reviews = Review.query.order_by(Review.created_at.desc()).all() #fetch all reviews, newest first
+     return render_template("home.html", reviews=reviews) #render home template with reviews
+
+#run app, keep at bottom
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
